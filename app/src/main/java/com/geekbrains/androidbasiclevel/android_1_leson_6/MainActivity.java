6fc7740ee63106485fbdec0fdec7f3834047940f
pospelovav <shnurokvtaige@yandex.ru>
2021-01-20 21:31:51 +0300

    new project

---
+++ app/src/main/java/com/geekbrains/androidbasiclevel/android_1_leson_6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geekbrains.androidbasiclevel.android_1_leson_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
